3e8475ac93163c755eef968f7f8185237daf9873
André Gil <contato@andregil.org>
2015-08-21 23:51:02 -0300

    Don't change touchpad tap behaviour for now

--- scripts/tools/setup/osx.sh
+++ scripts/tools/setup/osx.sh
@@ -151,9 +151,9 @@ defaults write com.apple.universalaccess reduceTransparency -bool true
 ###############################################################################
 
 # Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+# defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Trackpad: map bottom right corner to right-click
 # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
