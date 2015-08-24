cb7b696373ec681692bfc6c4b68b79a2830ba07f
André Gil <contato@andregil.org>
2015-08-24 09:46:15 -0300

    Fixing upgrade script environment refresh

--- scripts/tools/upgrade-dotfiles.sh
+++ scripts/tools/upgrade-dotfiles.sh
@@ -5,7 +5,8 @@ cd ~/.dotfiles
 printf '\033[0;34m%s\033[0m\n' "Upgrading dotfiles"
 git pull origin master
 sh ./oh-my-zsh/tools/upgrade.sh
-cd -
-source ~/.zshrc #>/dev/null 2>&1
+cd - >/dev/null 2>&1
+echo "\n${C_BLUE}Done!${C_RESTORE}"
 
-echo Done!
+env zsh
+. ~/.zshrc
