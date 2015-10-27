cb93822e6f81c04dbaa18ea5025e92a95e2a474e
André Gil <contato@andregil.org>
2015-10-27 10:43:07 -0200

    Making dock function start docker-machine when necessary

--- scripts/plugins/docker-machine/docker-machine.plugin.zsh
+++ scripts/plugins/docker-machine/docker-machine.plugin.zsh
@@ -1,6 +1,13 @@
 # Point docker to provided env (dev is default) and add to /etc/hosts as "docker"
 dock() {
+  if [[ $(docker-machine status ${1:-dev}) = *Stopped* ]]; then
+    docker-machine start ${1:-dev}
+  fi
 	eval "$(docker-machine env ${1:-dev})"
 	sudo sed -i '' '/docker$/d' /etc/hosts
 	echo "$(docker-machine ip ${1:-dev})  docker" | sudo tee -a /etc/hosts
+}
+
+undock() {
+  docker-machine stop ${1:-dev}
 }
