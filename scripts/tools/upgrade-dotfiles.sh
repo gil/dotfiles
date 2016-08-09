647a53cb18e539dc2309234096e0f82318740edb
Andre Gil <andre.gil@booking.com>
2016-08-09 09:26:59 +0200

    Updating upgrade script to allow local changes

--- scripts/tools/upgrade-dotfiles.sh
+++ scripts/tools/upgrade-dotfiles.sh
@@ -3,7 +3,8 @@
 
 cd ~/.dotfiles
 printf '\033[0;34m%s\033[0m\n' "Upgrading dotfiles"
-git pull origin master
+#git pull origin master
+git -c rebase.autoStash=true pull --rebase
 sh ./oh-my-zsh/tools/upgrade.sh
 cd - >/dev/null 2>&1
 echo "\n${C_BLUE}Done!${C_RESTORE}"
