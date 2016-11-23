8e25d4b33b2d4dcbdfa4cf80814aebc1e78a042c
André Gil <contato@andregil.org>
2016-11-23 23:04:04 +0100

    Updating rbenv before installing

--- scripts/tools/setup/rbenv.sh
+++ scripts/tools/setup/rbenv.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-cd ~/.rbenv && src/configure && make -C src
+cd ~/.rbenv
+git pull
+src/configure && make -C src
 ~/.rbenv/bin/rbenv init
 
 #refresh_dotfiles
