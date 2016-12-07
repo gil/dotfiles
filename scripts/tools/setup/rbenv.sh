6faefbe1e1b81f432d2a2e2f2c414ebf5365906b
André Gil <contato@andregil.org>
2016-12-07 12:45:01 +0100

    Removing source calls from rbenv script

--- scripts/tools/setup/rbenv.sh
+++ scripts/tools/setup/rbenv.sh
@@ -11,13 +11,9 @@ src/configure && make -C src
 #~/.rbenv/bin/rbenv init
 eval "$(rbenv init -)"
 
-source ~/.zshrc
-
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 cd ~/.rbenv/plugins/ruby-build
 git pull
 
 rbenv install 2.3.3
 rbenv global 2.3.3
-
-source ~/.zshrc
