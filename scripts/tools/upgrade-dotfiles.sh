7eeeb224aac14a23520c6668601d8ce351dc8f5c
André Gil <contato@andregil.org>
2018-08-15 22:15:08 +0200

    Adding zsh-autosuggestions and zsh-syntax-highlighting as submodules

--- scripts/tools/upgrade-dotfiles.sh
+++ scripts/tools/upgrade-dotfiles.sh
@@ -5,6 +5,7 @@ cd $OH_MY_GIL_SH
 printf '\033[0;34m%s\033[0m\n' "Upgrading dotfiles"
 #git pull origin master
 git -c rebase.autoStash=true pull --rebase
+git submodule update --init --recursive --rebase
 sh ./oh-my-zsh/tools/upgrade.sh
 cd - >/dev/null 2>&1
 printf "\n${C_BLUE}Done!${C_RESTORE}\n"
