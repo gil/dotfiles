65796d19a72d94da1bc53ba45780bb7a71192551
Andre Gil <andre.gil@booking.com>
2019-01-10 11:09:08 +0100

    Function to quickly setup tmux as I generally need for development

--- scripts/plugins/tmux/tmux.plugin.zsh
+++ scripts/plugins/tmux/tmux.plugin.zsh
@@ -1,1 +1,10 @@
 alias ta='tmux attach'
+
+function tmux-dev() {
+    tmux rename-window "$(basename "$PWD")"
+    tmux split-window -h
+    tmux split-window
+    tmux resize-pane -t 1 -x $(expr $(tmux display -p '#{window_width}') \* 35 \/ 100)
+    tmux resize-pane -t 2 -y $(expr $(tmux display -p '#{window_height}') \* 15 \/ 100)
+    tmux select-pane -t 0
+}
