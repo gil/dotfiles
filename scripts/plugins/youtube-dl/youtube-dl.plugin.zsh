3cf55810492fed4331aaf0300f6b19f568b0b9f8
André Gil <contato@andregil.org>
2019-07-05 00:22:29 +0200

    Fixing ytdl command

--- scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
+++ scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
@@ -1,4 +1,6 @@
 function ytdl {
+    #--merge-output-format mkv \
+    #--exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
     youtube-dl \
         --download-archive "youtube-dl-archive.log" \
         --ignore-errors \
@@ -6,11 +8,9 @@ function ytdl {
         --all-subs \
         --embed-subs \
         --embed-thumbnail \
-        #--merge-output-format mkv \
         --merge-output-format mp4 \
         --output "%(uploader)s - %(title)s - %(id)s.%(ext)s" \
         --external-downloader aria2c \
         --external-downloader-args "-c -j 4 -x 4 -s 4 -k 5M" \
-        #--exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
         "$@"
 }
