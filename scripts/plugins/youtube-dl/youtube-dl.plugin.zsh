67bbb401544e3eb3b268e385d99381fb084461d5
André Gil <contato@andregil.org>
2019-07-05 00:20:08 +0200

    Changing ytdl to mp4 and to use aria2c

--- scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
+++ scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
@@ -6,8 +6,11 @@ function ytdl {
         --all-subs \
         --embed-subs \
         --embed-thumbnail \
-        --merge-output-format mkv \
+        #--merge-output-format mkv \
+        --merge-output-format mp4 \
         --output "%(uploader)s - %(title)s - %(id)s.%(ext)s" \
-        --exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
+        --external-downloader aria2c \
+        --external-downloader-args "-c -j 4 -x 4 -s 4 -k 5M" \
+        #--exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
         "$@"
 }
