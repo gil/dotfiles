a0f87e234381a9ae8f33cc0402136e667d6b3607
André Gil <contato@andregil.org>
2019-09-24 23:17:20 +0200

    Updating youtube-dl script

--- scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
+++ scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
@@ -1,6 +1,4 @@
 function ytdl {
-    #--merge-output-format mkv \
-    #--exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
     youtube-dl \
         --download-archive "youtube-dl-archive.log" \
         --ignore-errors \
@@ -8,9 +6,10 @@ function ytdl {
         --all-subs \
         --embed-subs \
         --embed-thumbnail \
-        --merge-output-format mp4 \
+        --merge-output-format mkv \
         --output "%(uploader)s - %(title)s - %(id)s.%(ext)s" \
         --external-downloader aria2c \
-        --external-downloader-args "-c -j 4 -x 4 -s 4 -k 5M" \
+        --external-downloader-args "-c -j 4 -x 4 -s 4 -k 5M --file-allocation=none" \
+        --exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
         "$@"
 }
