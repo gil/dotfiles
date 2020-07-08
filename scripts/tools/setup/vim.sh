625b9ad00c5dfd077b033292b0137998a9554ea5
Andre Gil <contato@andregil.org>
2020-07-08 23:33:47 +0200

    Organizing the messy vim/tmux setup script

--- scripts/tools/setup/vim.sh
+++ scripts/tools/setup/vim.sh
@@ -1,97 +1,62 @@
 #!/usr/bin/env zsh
 
-printf "${C_BLUE}Looking for an existing .vimrc...${C_RESTORE}\n"
-if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
-  printf "${C_YELLOW}Found ~/.vimrc.${C_RESTORE} ${C_GREEN}Backing up to ~/.vimrc.backup${C_RESTORE}\n"
-  mv ~/.vimrc ~/.vimrc.backup;
-fi
+function _symlinkIfNeeded {
+  printf "${C_BLUE}Checking if $1 is right...${C_RESTORE}\n"
+  if [ $1 -ef $2 ]; then
+    printf "${C_GREEN}It is!${C_RESTORE}\n"
+  else
+    if [ -f $1 ] || [ -d $1 ] || [ -L $1 ]; then
+      backup_path=$1.backup-$(date +%Y%m%d-%H%M%S)
+      printf "${C_YELLOW}Found an old $1.${C_RESTORE} ${C_GREEN}Backing up to $backup_path${C_RESTORE}\n"
+      mv $1 $backup_path;
+    fi
+    printf "${C_BLUE}Creating $1 symlink...${C_RESTORE}\n"
+    ln -s $2 $1
+    printf "${C_GREEN}Done!${C_RESTORE}\n"
+  fi
+}
 
-printf "${C_BLUE}Creating .vimrc symlink...${C_RESTORE}\n"
-ln -s $OH_MY_GIL_SH/vim/.vimrc ~/.vimrc
+function _createDirectoryIfNeeded {
+  printf "${C_BLUE}Checking if $1 dir exists...${C_RESTORE}\n"
+  if [ -d $1 ]; then
+    printf "${C_GREEN}It does!${C_RESTORE}\n"
+  else
+    printf "${C_BLUE}Creating $1 dir...${C_RESTORE}\n"
+    mkdir -p $1
+    printf "${C_GREEN}Done!${C_RESTORE}\n"
+  fi
+}
 
-if [ ! -d ~/.vim ]; then
-  printf "${C_BLUE}Creating ~/.vim dir...${C_RESTORE}\n"
-  mkdir ~/.vim/
-fi
+### Vim/Neovim ###
 
-printf "${C_BLUE}Looking for an existing ~/.vim/ftplugin directory...${C_RESTORE}\n"
-if [ -d ~/.vim/ftplugin ] || [ -h ~/.vim/ftplugin ]; then
-  printf "${C_YELLOW}Found ~/.vim/ftplugin.${C_RESTORE} ${C_GREEN}Backing up to ~/.vim/ftplugin.backup${C_RESTORE}\n"
-  mv ~/.vim/ftplugin ~/.vim/ftplugin.backup;
-fi
+_symlinkIfNeeded ~/.vimrc $OH_MY_GIL_SH/vim/.vimrc
+_createDirectoryIfNeeded ~/.vim/
+_symlinkIfNeeded ~/.vim/ftplugin $OH_MY_GIL_SH/vim/ftplugin/
+_symlinkIfNeeded ~/.vim/UltiSnips $OH_MY_GIL_SH/vim/UltiSnips/
+_symlinkIfNeeded ~/.vim/spell $OH_MY_GIL_SH/vim/spell/
+_createDirectoryIfNeeded ~/.config/nvim
+_symlinkIfNeeded ~/.config/nvim/init.vim $OH_MY_GIL_SH/vim/init.vim
 
-printf "${C_BLUE}Creating ~/.vim/ftplugin symlink...${C_RESTORE}\n"
-ln -s $OH_MY_GIL_SH/vim/ftplugin/ ~/.vim/ftplugin
-
-printf "${C_BLUE}Looking for an existing ~/.vim/UltiSnips directory...${C_RESTORE}\n"
-if [ -d ~/.vim/UltiSnips ] || [ -h ~/.vim/UltiSnips ]; then
-  printf "${C_YELLOW}Found ~/.vim/UltiSnips.${C_RESTORE} ${C_GREEN}Backing up to ~/.vim/UltiSnips.backup${C_RESTORE}\n"
-  mv ~/.vim/UltiSnips ~/.vim/UltiSnips.backup;
-fi
-
-printf "${C_BLUE}Creating ~/.vim/UltiSnips symlink...${C_RESTORE}\n"
-ln -s $OH_MY_GIL_SH/vim/UltiSnips/ ~/.vim/UltiSnips
-
-printf "${C_BLUE}Looking for an existing ~/.vim/spell directory...${C_RESTORE}\n"
-if [ -d ~/.vim/spell ] || [ -h ~/.vim/spell ]; then
-  printf "${C_YELLOW}Found ~/.vim/spell.${C_RESTORE} ${C_GREEN}Backing up to ~/.vim/spell.backup${C_RESTORE}\n"
-  mv ~/.vim/spell ~/.vim/spell.backup;
-fi
-
-printf "${C_BLUE}Creating ~/.vim/spell symlink...${C_RESTORE}\n"
-ln -s $OH_MY_GIL_SH/vim/spell/ ~/.vim/spell
-
-#if [ -d ~/.vim/bundle/Vundle.vim ]; then
-    #printf "${C_BLUE}Updating Vundle...${C_RESTORE}\n"
-    #cd ~/.vim/bundle/Vundle.vim
-    #git pull origin master
-    #cd -
-#else
-    #printf "${C_BLUE}Cloning Vundle...${C_RESTORE}\n"
-    #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#fi
-
-#printf "${C_BLUE}Looking for an existing .ctags...${C_RESTORE}\n"
-#if [ -f ~/.ctags ] || [ -h ~/.ctags ]; then
-  #printf "${C_YELLOW}Found ~/.ctags.${C_RESTORE} ${C_GREEN}Backing up to ~/.ctags.backup${C_RESTORE}\n"
-  #mv ~/.ctags ~/.ctags.backup;
-#fi
-
-#printf "${C_BLUE}Creating .ctags symlink...${C_RESTORE}\n"
-#ln -s $OH_MY_GIL_SH/vim/.ctags ~/.ctags
-
-if [ ! -f ~/.config/nvim/init.vim ]; then
-  printf "${C_BLUE}Linking nvim config...${C_RESTORE}\n"
-  mkdir -p ~/.config/nvim
-  ln -s $OH_MY_GIL_SH/vim/init.vim ~/.config/nvim/init.vim
-fi
-
-printf "${C_BLUE}Installing plugins...${C_RESTORE}\n"
+printf "${C_BLUE}Installing Vim plugins...${C_RESTORE}\n"
 vim +PlugInstall +qall
 
-printf "${C_BLUE}Looking for an existing .tmux.conf...${C_RESTORE}\n"
-if [ -f ~/.tmux.conf ] || [ -h ~/.tmux.conf ]; then
-  printf "${C_YELLOW}Found ~/.tmux.conf.${C_RESTORE} ${C_GREEN}Backing up to ~/.tmux.conf.backup${C_RESTORE}\n"
-  mv ~/.tmux.conf ~/.tmux.conf.backup;
-fi
+### TMUX ###
 
-printf "${C_BLUE}Creating .tmux.conf symlink...${C_RESTORE}\n"
-ln -s $OH_MY_GIL_SH/vim/.tmux.conf ~/.tmux.conf
+_symlinkIfNeeded ~/.tmux.conf $OH_MY_GIL_SH/vim/.tmux.conf
 
 if [ -d ~/.tmux/plugins/tpm ]; then
     printf "${C_BLUE}Updating Tmux Plugin Manager...${C_RESTORE}\n"
     cd ~/.tmux/plugins/tpm
-    git pull origin master
+    git pull --rebase origin master
     cd -
 else
     printf "${C_BLUE}Cloning Tmux Plugin Manager...${C_RESTORE}\n"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-if [ ! -d ~/.config ]; then
-  printf "${C_YELLOW}Creating ~/.config directory.${C_RESTORE}\n"
-  mkdir ~/.config
-fi
-
-#printf "${C_BLUE}Installing Tmuxinator...${C_RESTORE}\n"
-#gem install tmuxinator
+printf "${C_BLUE}Installing new Tmux Plugins...${C_RESTORE}\n"
+~/.tmux/plugins/tpm/bin/install_plugins
+printf "${C_BLUE}Cleaning unused Tmux Plugins...${C_RESTORE}\n"
+~/.tmux/plugins/tpm/bin/clean_plugins
+printf "${C_BLUE}Updating Tmux Plugins...${C_RESTORE}\n"
+~/.tmux/plugins/tpm/bin/update_plugins all
