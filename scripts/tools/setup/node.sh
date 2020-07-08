03ad1e31fc9cd568ae7baba98c2230a84ea2046b
Andre Gil <contato@andregil.org>
2020-07-08 23:53:23 +0200

    Removing old NPM setup script

--- scripts/tools/setup/node.sh
+++ scripts/tools/setup/node.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env zsh
 
+# _npmInstallOrUpdate() {
+  #printf "\n${C_PURPLE}[NPM] ${C_GREEN}Checking for package \"$1\"...${C_RESTORE}\n"
+  #if npm list --depth 0 --global $1 > /dev/null 2>&1 ; then
+    #printf "${C_PURPLE}[NPM] ${C_BLUE}Package \"$1\" found! Updating...${C_RESTORE}\n"
+    #npm update --global $1
+  #else
+    #printf "${C_PURPLE}[NPM] ${C_BLUE}Package \"$1\" not found! Installing...${C_RESTORE}\n"
+    #npm install --global $1
+  #fi
+#}
+
 #####
 ## Install/Update NVM
 #####
