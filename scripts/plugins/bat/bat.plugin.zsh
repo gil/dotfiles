66981354e40f796500b45f4d3e726a203abaccf6
Andre Gil <contato@andregil.org>
2020-08-04 00:34:23 +0200

    Applying Bat theme

---
+++ scripts/plugins/bat/bat.plugin.zsh
@@ -0,0 +1,10 @@
+if [ ! -d ~/.config/bat ]; then
+  mkdir -p ~/.config/bat
+fi
+
+if [ ! -d ~/.config/bat/themes ]; then
+  ln -s $OH_MY_GIL_SH/scripts/themes/base16-themes/output/base16-textmate/Themes/ themes
+fi
+
+# If it's a new theme, run: bat cache --build
+export BAT_THEME="base16-gruvbox-dark-medium"
