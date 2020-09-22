314bf441af6ab834e0e768b97efff98eee80d199
Andre Gil <andre.gil@booking.com>
2020-09-22 14:36:58 +0200

    Getting rid of iStats dependency for bitbar

--- scripts/tools/bitbar/load_temp_fan.5s.sh
+++ scripts/tools/bitbar/load_temp_fan.5s.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-#
-# <bitbar.title>Load Average/Temperature/Fan Speed</bitbar.title>
+# <bitbar.title>Load Average + Temperature</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
-# <bitbar.author>Ivan Kovnatsky</bitbar.author>
-# <bitbar.author.github>sevenfourk</bitbar.author.github>
-# <bitbar.desc>This plugin displays the current CPU load, CPU temperature, Fan speed (requires iStats ruby gem)</bitbar.desc>
-# <bitbar.image>https://i.imgur.com/y3ytf1l.png</bitbar.image>
-# <bitbar.abouturl>https://github.com/matryer/bitbar-plugins/blob/master/System/load_temp_fan.10s.sh</bitbar.abouturl>
-#
-# iStats -- https://github.com/Chris911/iStats
-# gem install iStats
-#
-# Adapted by Andre Gil <https://github.com/gil/>
-#
+# <bitbar.author>Andre Gil</bitbar.author>
+# <bitbar.author.github>gil</bitbar.author.github>
+# <bitbar.desc>This plugin displays the current CPU load and CPU temperature without dependencies.</bitbar.desc>
 
-#load=$(uptime|sed 's/.*ages: //')
-#load=$(uptime | sed 's/.*ages: //' | awk '{print $1" "$2}')
 load=$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{print int($3+$5)}')
-temp=$(/usr/local/bin/istats cpu | awk '{print int($3)}') # | cut -d. -f1
-# fan=$(/usr/local/bin/istats fan speed|awk '{print $4, $5}')
+temp=$(sysctl machdep.xcpm.cpu_thermal_level | awk -F'[ ]' '{print $2}')
 
 echo ":rocket: $load% :fire: $tempº" #, $fan"
 
