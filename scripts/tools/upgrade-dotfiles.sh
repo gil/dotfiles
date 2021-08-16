2e39f35b9b5ff65a6295a2d1e5c955d0fcd21273
Andre Gil <andre.gil@booking.com>
2021-08-16 15:03:48 +0200

    better upgrade script and upgrading submodules

--- scripts/tools/upgrade-dotfiles.sh
+++ scripts/tools/upgrade-dotfiles.sh
@@ -2,13 +2,17 @@
 #set -e
 
 cd $OH_MY_GIL_SH
-printf '\033[0;34m%s\033[0m\n' "Upgrading dotfiles"
-#git pull origin master
+printf '\033[0;34m%s\033[0m\n' "Upgrading dotfiles..."
 git -c rebase.autoStash=true pull --rebase
-git submodule update --init --recursive --rebase
+
+printf '\033[0;34m%s\033[0m\n' "Upgrading submodules..."
+git submodule update --init --remote --recursive --rebase
+
+printf '\033[0;34m%s\033[0m\n' "Running oh-my-zsh upgrade script..."
 sh ./oh-my-zsh/tools/upgrade.sh
 cd - >/dev/null 2>&1
 
+printf '\033[0;34m%s\033[0m\n' "Generate bar theme cache..."
 if hash bat 2>/dev/null; then
   bat cache --build
 fi
