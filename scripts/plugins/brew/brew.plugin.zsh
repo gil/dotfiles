6ffa2a17e87520f8297199b6ef2d064f591ef9b7
Andre Gil <contato@andregil.org>
2021-11-02 22:11:23 +0100

    testing some new brew defaults

--- scripts/plugins/brew/brew.plugin.zsh
+++ scripts/plugins/brew/brew.plugin.zsh
@@ -2,3 +2,6 @@
 # alias bubo='brew update && brew outdated'
 # alias bubc='brew upgrade && brew cleanup'
 # alias bubu='bubo && bubc'
+
+export HOMEBREW_NO_INSTALL_UPGRADE=1
+export HOMEBREW_NO_AUTO_UPDATE=1
