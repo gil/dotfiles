37e51d8079abd7dd6d4d46621ef6296b041b787b
Andre Gil <contato@andregil.org>
2023-07-18 21:20:26 +0200

    better InconsolataGo patched with NerdFont

--- scripts/tools/setup/fonts-and-themes.sh
+++ scripts/tools/setup/fonts-and-themes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
-cp "$OH_MY_GIL_SH/scripts/tools/assets/InconsolataGo Nerd Font Complete [14].ttf" ~/Library/Fonts
+cp "$OH_MY_GIL_SH/scripts/tools/assets/InconsolataGoNerdFont-Regular.ttf" ~/Library/Fonts
 open $OH_MY_GIL_SH/scripts/tools/assets/Solarized\ Dark\ 256.terminal
