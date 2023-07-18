858e12bf9324154eb1e833ab63161750e9ba4e20
Andre Gil <contato@andregil.org>
2023-07-18 22:25:22 +0200

    forgot one comment

--- vim/init.lua
+++ vim/init.lua
@@ -47,6 +47,7 @@ vim.keymap.set('i', 'jj', '<Esc>') -- repeated jj is same as <Esc> to exist inse
 -- Plugins
 --
 
+-- Install lazy.nvim if not available
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
