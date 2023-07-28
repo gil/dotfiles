06e5b120692a74d56a4d8e22d934ac44dc142ed9
Andre Gil <andre.gil@booking.com>
2023-07-28 16:30:21 +0200

    better nvm lazy load script

--- scripts/plugins/nvm/nvm.plugin.zsh
+++ scripts/plugins/nvm/nvm.plugin.zsh
@@ -1,29 +1,72 @@
-if [ -f ~/.nvm/nvm.sh ] && [ "$NVM_LAZY_ALIAS_SET" -ne "1" ]; then
-    ##
-    # This will try to lazy load NVM only when needed, since initializing it could be slow
-    ###
-    export NVM_LAZY_ALIAS_SET=1
+NODE_GLOBALS="`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq | xargs echo -n`"
 
-    declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
-
-    if [ -d ~/.yarn/bin ]; then
-        NODE_GLOBALS+=(`find ~/.yarn/bin -maxdepth 1 -type l -wholename '*' | xargs -n1 basename | sort | uniq`)
-    fi
-
-    NODE_GLOBALS+=("node")
-    NODE_GLOBALS+=("nvm")
-    NODE_GLOBALS+=("vim")
-    NODE_GLOBALS+=("nvim")
-    #NODE_GLOBALS+=("yarn")
-
-    load_nvm () {
-        export NVM_DIR=~/.nvm
-        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-        [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-        export PATH="$PATH:$(yarn global bin)"
-    }
-
-    for cmd in "${NODE_GLOBALS[@]}"; do
-        eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
-    done
+if [ -d ~/.yarn/bin ]; then
+  NODE_GLOBALS="$NODE_GLOBALS `find ~/.yarn/bin -maxdepth 1 -type l -wholename '*' | xargs -n1 basename | sort | uniq | xargs echo -n`"
 fi
+
+NODE_GLOBALS="$NODE_GLOBALS vim nvim"
+NODE_GLOBALS="$(echo $NODE_GLOBALS | sed -E 's/pnpm|npm|npx|yarn//g')" # remove duplicates from below
+
+zstyle ':omz:plugins:nvm' lazy yes
+zstyle ':omz:plugins:nvm' lazy-cmd $NODE_GLOBALS
+
+########################
+## Original script goes below
+##
+## Just remember to include this inside of lazy loading thingy:
+##   export PATH="$PATH:$(yarn global bin)"
+########################
+
+# See https://github.com/nvm-sh/nvm#installation-and-update
+if [[ -z "$NVM_DIR" ]]; then
+  if [[ -d "$HOME/.nvm" ]]; then
+    export NVM_DIR="$HOME/.nvm"
+  elif [[ -d "${XDG_CONFIG_HOME:-$HOME/.config}/nvm" ]]; then
+    export NVM_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/nvm"
+  elif (( $+commands[brew] )); then
+    NVM_HOMEBREW="${NVM_HOMEBREW:-${HOMEBREW_PREFIX:-$(brew --prefix)}/opt/nvm}"
+    if [[ -d "$NVM_HOMEBREW" ]]; then
+      export NVM_DIR="$NVM_HOMEBREW"
+    fi
+  fi
+fi
+
+# Don't try to load nvm if command already available
+# Note: nvm is a function so we need to use `which`
+which nvm &>/dev/null && return
+
+if [[ -z "$NVM_DIR" ]] || [[ ! -f "$NVM_DIR/nvm.sh" ]]; then 
+  return
+fi
+
+if zstyle -t ':omz:plugins:nvm' lazy && \
+  ! zstyle -t ':omz:plugins:nvm' autoload; then
+  # Call nvm when first using nvm, node, npm, pnpm, yarn or other commands in lazy-cmd
+  zstyle -a ':omz:plugins:nvm' lazy-cmd nvm_lazy_cmd
+  eval "
+    function nvm node npm npx pnpm yarn $nvm_lazy_cmd {
+      unfunction nvm node npm npx pnpm yarn $nvm_lazy_cmd
+      # Load nvm if it exists in \$NVM_DIR
+      [[ -f \"\$NVM_DIR/nvm.sh\" ]] && source \"\$NVM_DIR/nvm.sh\"
+      export PATH=\"\$PATH:\$(yarn global bin)\"
+      \"\$0\" \"\$@\"
+    }
+  "
+  unset nvm_lazy_cmd
+else
+  source "$NVM_DIR/nvm.sh"
+fi
+
+# Load nvm bash completion
+for nvm_completion in "$NVM_DIR/bash_completion" "$NVM_HOMEBREW/etc/bash_completion.d/nvm"; do
+  if [[ -f "$nvm_completion" ]]; then
+    # Load bashcompinit
+    autoload -U +X bashcompinit && bashcompinit
+    # Bypass compinit call in nvm bash completion script. See:
+    # https://github.com/nvm-sh/nvm/blob/4436638/bash_completion#L86-L93
+    ZSH_VERSION= source "$nvm_completion"
+    break
+  fi
+done
+
+unset NVM_HOMEBREW nvm_completion
