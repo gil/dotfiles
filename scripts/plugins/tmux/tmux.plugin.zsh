803a53ee0aecc2e58e0e2773d2ac18552d6d854b
Andre Gil <andre.gil@booking.com>
2023-12-07 15:12:14 +0100

    run vim on tmux-dev

--- scripts/plugins/tmux/tmux.plugin.zsh
+++ scripts/plugins/tmux/tmux.plugin.zsh
@@ -7,4 +7,5 @@ function tmux-dev() {
     tmux resize-pane -t 1 -x $(expr $(tmux display -p '#{window_width}') \* 35 \/ 100)
     tmux resize-pane -t 2 -y $(expr $(tmux display -p '#{window_height}') \* 15 \/ 100)
     tmux select-pane -t 0
+    nvim
 }
