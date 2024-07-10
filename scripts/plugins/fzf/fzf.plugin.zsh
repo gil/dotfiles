df66113ae7bf15c76da5c4da1bf1d19f1a428b05
Andre Gil <andre.gil@booking.com>
2024-07-10 15:33:30 +0200

    better default command for FZF with fd and rg

--- scripts/plugins/fzf/fzf.plugin.zsh
+++ scripts/plugins/fzf/fzf.plugin.zsh
@@ -1,5 +1,7 @@
 export FZF_DEFAULT_COMMAND='(
+    fd --type f --strip-cwd-prefix --hidden --follow --exclude .git ||
     git ls-tree -r --name-only HEAD ||
+    rg --files-with-matches --hidden --ignore .git -g "" ||
     ag --hidden --ignore .git -g "" ||
     ack --hidden --ignore .git -g "" ||
     find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed s/^..//
