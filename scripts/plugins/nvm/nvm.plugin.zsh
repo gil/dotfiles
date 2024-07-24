2b913c02385b5fa455ed91ab627e5341ac0ca022
Andre Gil <andre.gil@booking.com>
2024-07-24 15:37:39 +0200

    upgrade nvm plugin and fix issue with workspaces

--- scripts/plugins/nvm/nvm.plugin.zsh
+++ scripts/plugins/nvm/nvm.plugin.zsh
@@ -18,8 +18,15 @@ zstyle ':omz:plugins:nvm' lazy-cmd $NODE_GLOBALS
 ## Original script goes below
 ##
 ## Just remember to include this inside of lazy loading thingy:
-##   export PATH="$PATH:$(yarn global bin)"
+##   export PATH=\"\$PATH:\$(yarn global bin)\"
+##
+## And the cd $HOME / cd - thingy to avoid breaking on workspaces
+##
 ########################
+
+# Don't try to load nvm if command already available
+# Note: nvm is a function so we need to use `which`
+which nvm &>/dev/null && return
 
 # See https://github.com/nvm-sh/nvm#installation-and-update
 if [[ -z "$NVM_DIR" ]]; then
@@ -35,42 +42,85 @@ if [[ -z "$NVM_DIR" ]]; then
   fi
 fi
 
-# Don't try to load nvm if command already available
-# Note: nvm is a function so we need to use `which`
-which nvm &>/dev/null && return
-
-if [[ -z "$NVM_DIR" ]] || [[ ! -f "$NVM_DIR/nvm.sh" ]]; then 
+if [[ -z "$NVM_DIR" ]] || [[ ! -f "$NVM_DIR/nvm.sh" ]]; then
   return
 fi
 
-if zstyle -t ':omz:plugins:nvm' lazy && \
-  ! zstyle -t ':omz:plugins:nvm' autoload; then
-  # Call nvm when first using nvm, node, npm, pnpm, yarn or other commands in lazy-cmd
+function _omz_load_nvm_completion {
+  local _nvm_completion
+  # Load nvm bash completion
+  for _nvm_completion in "$NVM_DIR/bash_completion" "$NVM_HOMEBREW/etc/bash_completion.d/nvm"; do
+    if [[ -f "$_nvm_completion" ]]; then
+      # Load bashcompinit
+      autoload -U +X bashcompinit && bashcompinit
+      # Bypass compinit call in nvm bash completion script. See:
+      # https://github.com/nvm-sh/nvm/blob/4436638/bash_completion#L86-L93
+      ZSH_VERSION= source "$_nvm_completion"
+      break
+    fi
+  done
+  unfunction _omz_load_nvm_completion
+}
+
+function _omz_setup_autoload {
+  if ! zstyle -t ':omz:plugins:nvm' autoload; then
+    unfunction _omz_setup_autoload
+    return
+  fi
+
+  # Autoload nvm when finding a .nvmrc file in the current directory
+  # Adapted from: https://github.com/nvm-sh/nvm#zsh
+  function load-nvmrc {
+    local nvmrc_path="$(nvm_find_nvmrc)"
+    local nvm_silent=""
+    zstyle -t ':omz:plugins:nvm' silent-autoload && nvm_silent="--silent"
+
+    if [[ -n "$nvmrc_path" ]]; then
+      local nvmrc_node_version=$(nvm version $(command cat "$nvmrc_path" | tr -dc '[:print:]'))
+
+      if [[ "$nvmrc_node_version" = "N/A" ]]; then
+        nvm install
+      elif [[ "$nvmrc_node_version" != "$(nvm version)" ]]; then
+        nvm use $nvm_silent
+      fi
+    elif [[ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ]] && [[ "$(nvm version)" != "$(nvm version default)" ]]; then
+      [[ -z $nvm_silent ]] && echo "Reverting to nvm default version"
+
+      nvm use default $nvm_silent
+    fi
+  }
+
+  autoload -U add-zsh-hook
+  add-zsh-hook chpwd load-nvmrc
+
+  load-nvmrc
+  unfunction _omz_setup_autoload
+}
+
+if zstyle -t ':omz:plugins:nvm' lazy; then
+  # Call nvm when first using nvm, node, npm, pnpm, yarn, corepack or other commands in lazy-cmd
   zstyle -a ':omz:plugins:nvm' lazy-cmd nvm_lazy_cmd
+  nvm_lazy_cmd=(nvm node npm npx pnpm yarn corepack $nvm_lazy_cmd) # default values
   eval "
-    function nvm node npm npx pnpm yarn $nvm_lazy_cmd {
-      unfunction nvm node npm npx pnpm yarn $nvm_lazy_cmd
+    function $nvm_lazy_cmd {
+      for func in $nvm_lazy_cmd; do
+        if (( \$+functions[\$func] )); then
+          unfunction \$func
+        fi
+      done
+      cd "$HOME"
       # Load nvm if it exists in \$NVM_DIR
       [[ -f \"\$NVM_DIR/nvm.sh\" ]] && source \"\$NVM_DIR/nvm.sh\"
+      _omz_load_nvm_completion
+      _omz_setup_autoload
       export PATH=\"\$PATH:\$(yarn global bin)\"
+      cd -
       \"\$0\" \"\$@\"
     }
   "
   unset nvm_lazy_cmd
 else
   source "$NVM_DIR/nvm.sh"
+  _omz_load_nvm_completion
+  _omz_setup_autoload
 fi
-
-# Load nvm bash completion
-for nvm_completion in "$NVM_DIR/bash_completion" "$NVM_HOMEBREW/etc/bash_completion.d/nvm"; do
-  if [[ -f "$nvm_completion" ]]; then
-    # Load bashcompinit
-    autoload -U +X bashcompinit && bashcompinit
-    # Bypass compinit call in nvm bash completion script. See:
-    # https://github.com/nvm-sh/nvm/blob/4436638/bash_completion#L86-L93
-    ZSH_VERSION= source "$nvm_completion"
-    break
-  fi
-done
-
-unset NVM_HOMEBREW nvm_completion
