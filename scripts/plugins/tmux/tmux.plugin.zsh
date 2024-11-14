22e685eb04bae9b19bc5a8ca7949a88c900b66e8
Andre Gil <andre.gil@booking.com>
2024-11-14 12:54:30 +0100

    tmux: better helper alias/function

--- scripts/plugins/tmux/tmux.plugin.zsh
+++ scripts/plugins/tmux/tmux.plugin.zsh
@@ -1,11 +1,8 @@
-alias ta='tmux attach'
+alias ta='tmux attach || tmux'
 
 function tmux-dev() {
-    tmux rename-window "$(basename "$PWD")"
-    tmux split-window -h
-    tmux split-window
-    tmux resize-pane -t 1 -x $(expr $(tmux display -p '#{window_width}') \* 35 \/ 100)
-    tmux resize-pane -t 2 -y $(expr $(tmux display -p '#{window_height}') \* 15 \/ 100)
-    tmux select-pane -t 0
-    nvim
+  tmux rename-session "$(basename "$PWD")"
+  tmux new-window -c "$PWD"
+  tmux select-window -t 0
+  nvim
 }
