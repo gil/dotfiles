bd8924d80f3c7dca73123453eb12ff1920c56245
Andre Gil <contato@andregil.org>
2024-12-19 21:54:19 +0100

    yt-dlp: highest quality

--- scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
+++ scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
@@ -1,6 +1,22 @@
+# --external-downloader aria2c \
+# --external-downloader-args "-c -j 4 -x 4 -s 4 -k 5M --file-allocation=none" \
+
 function ytdl {
-    # --external-downloader aria2c \
-    # --external-downloader-args "-c -j 4 -x 4 -s 4 -k 5M --file-allocation=none" \
+    yt-dlp \
+        --download-archive "youtube-dl-archive.log" \
+        --ignore-errors \
+        --add-metadata \
+        --all-subs \
+        --embed-subs \
+        --embed-thumbnail \
+        --format "bestvideo+bestaudio/best" \
+        --merge-output-format mkv \
+        --output "%(title).150s - %(uploader)s [%(id)s].%(ext)s" \
+        --exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
+        "$@"
+}
+
+function ytdl-low {
 
     yt-dlp \
         --download-archive "youtube-dl-archive.log" \
