33436e38b3637be45331915ab356b3644d457476
Andre Gil <andre.gil@booking.com>
2025-01-29 10:43:50 +0100

    osx: tiny mistake

--- scripts/tools/setup/osx.sh
+++ scripts/tools/setup/osx.sh
@@ -62,7 +62,7 @@ defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0.15
 
 # Dock: Don't show recently used apps
-defaults write com.apple.dock show-recents -bool true
+defaults write com.apple.dock show-recents -bool false
 
 # Screenshot: No shadow
 defaults write com.apple.screencapture disable-shadow -bool true
