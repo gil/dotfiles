6ef8d2f5587ff7bf38651a7aeae12ce60de2878a
Andre Gil <contato@andregil.org>
2025-03-11 22:19:16 +0100

    yt-dlp: mp4

--- scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
+++ scripts/plugins/youtube-dl/youtube-dl.plugin.zsh
@@ -1,5 +1,10 @@
+# Use aria2c as external downloader
 # --external-downloader aria2c \
 # --external-downloader-args "-c -j 4 -x 4 -s 4 -k 5M --file-allocation=none" \
+
+# Covert to mkv
+# --merge-output-format mkv \
+# --exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
 
 function ytdl {
     yt-dlp \
@@ -10,9 +15,7 @@ function ytdl {
         --embed-subs \
         --embed-thumbnail \
         --format "bestvideo+bestaudio/best" \
-        --merge-output-format mkv \
         --output "%(title).150s - %(uploader)s [%(id)s].%(ext)s" \
-        --exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
         "$@"
 }
 
@@ -26,8 +29,6 @@ function ytdl-low {
         --embed-subs \
         --embed-thumbnail \
         --format "bestvideo[height<=?1080]+bestaudio/best[height<=?1080]/best" \
-        --merge-output-format mkv \
         --output "%(title).150s - %(uploader)s [%(id)s].%(ext)s" \
-        --exec "$OH_MY_GIL_SH/scripts/plugins/youtube-dl/embed_mkv_thumb.sh {}" \
         "$@"
 }
