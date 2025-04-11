967f2e14a0e1526dc90eae092748431dbb6c2f57
Andre Gil <andre.gil@booking.com>
2025-04-11 10:06:42 +0200

    node: don't let corepack touch package.json files

--- scripts/plugins/node/node.plugin.zsh
+++ scripts/plugins/node/node.plugin.zsh
@@ -3,3 +3,7 @@
 function node-docs {
   open_command "http://nodejs.org/docs/$(node --version)/api/all.html#all_$1"
 }
+
+# Corepace: https://github.com/nodejs/corepack/blob/main/README.md#environment-variables
+# Don't add packageManager automatically to package.json files that don't have it
+export COREPACK_ENABLE_AUTO_PIN=0
