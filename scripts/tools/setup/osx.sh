7e0ae81cf07b838b7c0c5597054db45d649319ce
Andre Gil <contato@andregil.org>
2025-10-03 22:21:48 +0200

    osx: group windows by app on expose

--- scripts/tools/setup/osx.sh
+++ scripts/tools/setup/osx.sh
@@ -67,6 +67,9 @@ defaults write com.apple.dock show-recents -bool false
 # Dock: Show app switcher on all displays
 defaults write com.apple.dock appswitcher-all-displays -bool true
 
+# Mission Control: Exposé -> Group windows by application
+defaults write com.apple.dock expose-group-apps -bool true
+
 # Screenshot: No shadow
 defaults write com.apple.screencapture disable-shadow -bool true
 
